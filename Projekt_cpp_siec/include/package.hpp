8259e3b7215297d301269ddd91f9abafdd555298
Lukasz Wach <lukaszwach@student.agh.edu.pl>
2023-12-08 21:59:14 +0100

    Poprawiony package.hpp i package.cpp

--- Projekt_cpp_siec/include/package.hpp
+++ Projekt_cpp_siec/include/package.hpp
@@ -2,14 +2,31 @@
 // Created by Szymon on 30.11.2023.
 //
 
-#ifndef PROJEKT_CPP_PACKAGE_HPP
-#define PROJEKT_CPP_PACKAGE_HPP
-#include <types.hpp>
-class Package{
-    public:
-    Package(ElementID id_): id(id_) {}
+#ifndef NETSIM_PACKAGE_HPP
+#define NETSIM_PACKAGE_HPP
 
-    private:
-    ElementID id;
+#include "types.hpp"
+#include <set>
+
+class Package
+{
+public:
+    Package();
+
+    explicit Package(ElementID id) : id_(id) { assigned_IDs.insert(id); }
+
+    Package(Package &&package) noexcept : id_{package.id_} {}
+
+    Package &operator=(Package &&package) noexcept;
+
+    ElementID get_id() const { return id_; }
+
+    ~Package();
+
+private:
+    static std::set<ElementID> assigned_IDs;
+    static std::set<ElementID> freed_IDs;
+    ElementID id_;
 };
-#endif //PROJEKT_CPP_PACKAGE_HPP
+
+#endif
--- Projekt_cpp_siec/src/package.cpp
+++ Projekt_cpp_siec/src/package.cpp
@@ -1,3 +1,42 @@
 //
 // Created by Szymon on 30.11.2023.
 //
+#include "package.hpp"
+
+std::set<ElementID> Package::assigned_IDs = {};
+std::set<ElementID> Package::freed_IDs = {};
+
+Package::Package()
+{
+    if (assigned_IDs.empty() && freed_IDs.empty())
+    {
+        id_ = 1;
+        assigned_IDs.insert(1);
+    }
+    else if (!freed_IDs.empty())
+    {
+        id_ = *freed_IDs.begin();
+        assigned_IDs.insert(id_);
+        freed_IDs.erase(id_);
+    }
+    else
+    {
+        id_ = *assigned_IDs.end() + 1;
+        assigned_IDs.insert(id_);
+    }
+}
+
+Package &Package::operator=(Package &&package) noexcept
+{
+    assigned_IDs.erase(this->id_);
+    freed_IDs.insert(this->id_);
+    this->id_ = package.id_;
+    assigned_IDs.insert(this->id_);
+    return *this;
+}
+
+Package::~Package()
+{
+    freed_IDs.insert(id_);
+    assigned_IDs.erase(id_);
+}
